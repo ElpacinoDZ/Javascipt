0c1e5d72857526805571cf14a7fcef2b2d697ded
ElpacinoDZ <ahmedmoundji2014@outlook.fr>
2025-08-11 00:50:31 +0200

    Changement de code

--- app-javascript.js
+++ app-javascript.js
@@ -1,18 +1,36 @@
-const listeMots = ["Cachalot", "Pétunia", "Serviette"];
-let score = 0;
+// Déclaration du tableau contenant la liste des mots proposés à l'utilisateur
+const listeMots = ['Cachalot', 'Pétunia', 'Serviette']
+const listePhrases = ["Pas de panique !", "La vie, l’univers et le reste", "Merci pour le poisson"]
+let score = 0
 
-let continuer = true;
-while (continuer) {
-    let motUtilisateur = prompt("Entrez le mot (ou tapez 'stop' pour arrêter) :");
+let choix = prompt("Tapez 1 pour jouer avec les mots, ou 2 pour jouer avec les phrases");
 
-    if (motUtilisateur === "stop") {
-        continuer = false;
-    } else if (listeMots.includes(motUtilisateur)) {
-        score++;
-        console.log("Bravo !");
-    } else {
-        console.log("Vous avez fait une erreur de frappe, recommencez !");
+while (choix !== "1" && choix !== "2")
+    {
+        choix = prompt("Tapez 1 pour jouer avec les mots, ou 2 pour jouer avec les phrases");
     }
-    console.log("Score actuel :", score);
+if (choix == "1")
+    {
+        for (let i= 0 ; i < listeMots.length; i++){
+    let motUtilisateur = prompt('Entrez le mot : ' + listeMots[i])
+    if (motUtilisateur === listeMots[i]) {
+    score++
+    }
 }
-console.log("Score final :", score);
+
+}
+else {
+    for (let i= 0 ; i < listePhrases.length; i++){
+    let motUtilisateur = prompt('Entrez le mot : ' + listePhrases[i])
+    if (motUtilisateur === listePhrases[i]) {
+    score++
+    }
+}
+}
+// Affichage du score de l'utilisateur
+
+if (choix == "1"){
+     console.log("Votre score est de " + score + " sur "+ listeMots.length)
+}else if (choix == "2")
+
+console.log("Votre score est de " + score + " sur "+ liste.length)
